98eedd47085e3eb22272eeca00afbe2aa53e8f76
luciuswilbert <luciuswilbert@gmail.com>
2025-10-22 10:29:41 +0700

    File Uploading

--- main.py
+++ main.py
@@ -1,42 +1,18 @@
-# Importing Libraries..
-from flask import Flask, render_template, request
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
-from wtforms import DecimalField, RadioField, SelectField, TextAreaField, FileField
-from wtforms.validators import InputRequired
-from werkzeug.security import generate_password_hash
+from distutils.log import debug
+from fileinput import filename
+from flask import *  
+app = Flask(__name__)  
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secretkey'
+@app.route('/')  
+def main():  
+    return render_template("index.html")  
 
+@app.route('/success', methods = ['POST'])  
+def success():  
+    if request.method == 'POST':  
+        f = request.files['file']
+        f.save(f.filename)  
+        return render_template("Acknowledgement.html", name = f.filename)  
 
-class MyForm(FlaskForm):
-    name = StringField('Name', validators=[InputRequired()])
-    password = PasswordField('Password', validators=[InputRequired()])
-    remember_me = BooleanField('Remember me')
-    salary = DecimalField('Salary', validators=[InputRequired()])
-    gender = RadioField('Gender', choices=[
-                        ('male', 'Male'), ('female', 'Female')])
-    country = SelectField('Country', choices=[('IN', 'India'), ('US', 'United States'),
-                                              ('UK', 'United Kingdom')])
-    message = TextAreaField('Message', validators=[InputRequired()])
-    photo = FileField('Photo')
-
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    form = MyForm()
-    if form.validate_on_submit():
-        name = form.name.data
-        password = form.password.data
-        remember_me = form.remember_me.data
-        salary = form.salary.data
-        gender = form.gender.data
-        country = form.country.data
-        message = form.message.data
-        photo = form.photo.data.filename
-        return f'Name: {name} <br> Password: {generate_password_hash(password)} <br> Remember me: {remember_me} <br> Salary: {salary} <br> Gender: {gender} <br> Country: {country} <br> Message: {message} <br> Photo: {photo}        <br > Remember me: {remember_me} < br > Salary: {salary} < br > Gender: {gender}        <br > Country: {country} < br > Message: {message} < br > Photo: {photo}'
-    return render_template('index.html', form=form)
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == '__main__':  
+    app.run(debug=True)

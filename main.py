1385784820572c0b8c3ae266180a06a172e88688
luciuswilbert <luciuswilbert@gmail.com>
2025-10-22 12:44:11 +0700

    Environment Settings

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
-from distutils.log import debug
-from fileinput import filename
-from flask import *  
-app = Flask(__name__)  
+from flask import Flask, render_template, request, flash
+from flask_wtf import FlaskForm, CSRFProtect
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
-@app.route('/')  
-def main():  
-    return render_template("index.html")  
+app = Flask(__name__)
+app.debug = True
+app.secret_key = 'your_secret_key'  # Required for CSRF protection
+csrf = CSRFProtect(app)  # Enable CSRF Protection
 
-@app.route('/success', methods = ['POST'])  
-def success():  
-    if request.method == 'POST':  
-        f = request.files['file']
-        f.save(f.filename)  
-        return render_template("Acknowledgement.html", name = f.filename)  
+# Creating a FlaskForm to manage CSRF properly
+class NameForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
-if __name__ == '__main__':  
-    app.run(debug=True)
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            name = form.name.data
+            flash(f'Hello {name} from Protected Form!', 'success')
+            return render_template('index.html', form=form)
+        else:
+            flash('CSRF Token Missing or Invalid!', 'danger')
+
+    return render_template('index.html', form=form)
+
+@app.route("/unprotected_form", methods=['POST'])
+def unprotected_form():
+    name = request.form.get('Name', '').strip()
+    if not name:
+        return "Error: Name is required!", 400
+    return f'Hello {name} from Unprotected Form!'
+
+if __name__ == '__main__':
+    app.run()

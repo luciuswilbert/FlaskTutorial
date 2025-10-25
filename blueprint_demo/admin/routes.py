5a3bd98ea919290f3910c2d2a1ba646761ce8c59
luciuswilbert <luciuswilbert@gmail.com>
2025-10-25 13:07:27 +0700

    Learning about Dockerization

--- blueprint_demo/admin/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, Blueprint
-
-admin_bp = Blueprint('admin', __name__)
-
-@admin_bp.route('/hello', methods=['GET'])
-def hello_admin():
-    return "Hello, Admin!"
--- blueprint_demo/user/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, jsonify, Blueprint
-
-user_bp = Blueprint('user', __name__)
-
-@user_bp.route('/hello', methods=['GET'])
-def hello_user():
-    return "Hello, User!"

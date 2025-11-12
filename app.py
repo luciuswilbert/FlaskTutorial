4e9b36d201dd13df076c089e2f219eea9786a510
luciuswilbert <luciuswilbert@gmail.com>
2025-11-12 11:22:00 +0700

    Done, going to learn the integration with DynamoDB

--- app.py
+++ app.py
@@ -11,3 +11,34 @@ books = [
 def get_books():
     return jsonify(books)
 
+@app.route('/books/<int:book_id>', methods=['GET'])
+def get_books_by_id(book_id):
+    book = next((book for book in books if book['id'] == book_id), any)
+    return jsonify(book)
+
+@app.route('/books', methods=['POST'])
+def add_book():
+    new_book = request.json
+    print(request.json)
+    books.append(new_book)
+    return jsonify(books)
+
+@app.route('/books/<int:book_id>', methods=['PUT'])
+def edit_book(book_id):
+    book = next((book for book in books if book['id'] == book_id), any)
+    if not book:
+        return jsonify({"error": "Book not Found"})
+    
+    data = request.json
+    print(data)
+    book.update(data)
+    return jsonify(book)
+
+@app.route('/books/<int:book_id>', methods=['DELETE'])
+def delete_book(book_id):
+    global books
+    books = [book for book in books if book['id'] != book_id]
+    return jsonify(books)
+
+if __name__ == '__main__':
+    app.run(debug=True)

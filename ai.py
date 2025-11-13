261f354b5549f3bfcb6fcf35a0ba89acac03b0ae
luciuswilbert <luciuswilbert@gmail.com>
2025-11-13 17:24:44 +0700

    AI Works

--- ai.py
+++ ai.py
@@ -8,13 +8,6 @@ load_dotenv()
 
 app = Flask(__name__)
 
-# client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
-
-# response = client.models.generate_content(
-#     model="gemini-2.5-flash", contents="Explain how AI works in a few words"
-# )
-# print(response.text)
-
 books = [
     {"id": 1, "title": "1984", "author": "George Orwell"},
     {"id": 2, "title": "To Kill a Mockingbird", "author": "Harper Lee"},
@@ -53,6 +46,32 @@ def delete_book(book_id):
     global books
     books = [book for book in books if book['id'] != book_id]
     return jsonify(books)
+
+@app.route('/suggest-books', methods=['GET'])
+def suggest_book():
+    global books
+    client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+    prompt = f"""
+        You are a book recommender.
+
+        Here is a list of books in JSON format:
+        {books}
+
+        Task:
+        - Choose exactly ONE book from this list only.
+        - Do NOT recommend any book that is not in the list.
+        - Explain briefly why you chose it.
+
+        Return the answer in plain text.
+        """
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", contents=prompt
+    )
+    response = response.text
+    print(books)
+    return jsonify({"response:": response})
 
 if __name__ == '__main__':
     app.run(debug=True)
